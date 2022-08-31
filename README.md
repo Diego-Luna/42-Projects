3e0491523e9549a2dee315fc178c90541c80aba8
Diego-Luna <diegofcolunalopez@gmail.com>
2022-08-31 16:57:45 -0500

    run get_next_line

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 42-Projects
 
+
 ## ft_printf
 run : clear && make clean && make all && gcc main.c libftprintf.a && ./a.out | cat -e
+
+
+## get_next_line
+
+run :  clear && make clean && make all && gcc -Wall -Wextra -Werror -D BUFFER_SIZE=26 main.c get_next_line.a && ./a.out test/test_0.txt | cat -e
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,29 +6,29 @@
 /*   By: anacamilalunalopez <anacamilalunalopez@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:30:44 by anacamilalu       #+#    #+#             */
-/*   Updated: 2022/08/31 16:24:17 by anacamilalu      ###   ########.fr       */
+/*   Updated: 2022/08/31 16:47:16 by anacamilalu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <unistd.h>
-#include "src/get_next_line.h"
+#include "get_next_line.h"
 
 int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
 
-	(void)argc;			// Casting argc to quiet the compiler's warnings.
-	fd = open(argv[1], O_RDONLY); 	// Open the file given as an argument at program launch
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
 	if (fd)
 	{
-		line = "";			// Initialize this variable to be able to use it in the following loop
+		line = "";
 		while (line != NULL)
 		{
 			line = get_next_line(fd);
-			printf("%s", line);	// We're omitting any \n in this printf because get_next_line is supposed to include the \n for each line
+			printf("%s", line);
 		}
 		fd = close(fd);
 	}
