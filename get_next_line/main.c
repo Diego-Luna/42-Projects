3af25788b44b7ed0009d34fc20166e71b3db4050
Diego-Luna <diegofcolunalopez@gmail.com>
2022-08-31 16:45:59 -0500

    cambio en organisacion

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,14 +6,14 @@
 /*   By: anacamilalunalopez <anacamilalunalopez@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:30:44 by anacamilalu       #+#    #+#             */
-/*   Updated: 2022/08/29 19:45:36 by anacamilalu      ###   ########.fr       */
+/*   Updated: 2022/08/31 16:24:17 by anacamilalu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <fcntl.h> 
 #include <unistd.h>
-#include "get_next_line.h"
+#include "src/get_next_line.h"
 
 int	main(int argc, char **argv)
 {
@@ -22,12 +22,15 @@ int	main(int argc, char **argv)
 
 	(void)argc;			// Casting argc to quiet the compiler's warnings.
 	fd = open(argv[1], O_RDONLY); 	// Open the file given as an argument at program launch
-	line = "";			// Initialize this variable to be able to use it in the following loop
-	while (line != NULL)
+	if (fd)
 	{
-		line = get_next_line(fd);
-		printf("%s", line);	// We're omitting any \n in this printf because get_next_line is supposed to include the \n for each line
+		line = "";			// Initialize this variable to be able to use it in the following loop
+		while (line != NULL)
+		{
+			line = get_next_line(fd);
+			printf("%s", line);	// We're omitting any \n in this printf because get_next_line is supposed to include the \n for each line
+		}
+		fd = close(fd);
 	}
-	fd = close(fd);
 	return (0);
 }
