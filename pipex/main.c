9fdb48f46ddf0eb48197d0419bd14168e15cda5b
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-04 14:26:28 -0400

    Probando conceptos del proyecto

--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,21 @@
 /*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 12:50:01 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/11/04 12:50:01 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:20:35 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int	main(int argc, char const *argv[])
+{
+	int	id;
+
+	id = fork();
+	printf("\nCurrent ID: %d, parent ID: %d", getpid(), getppid());
+	return (0);
+}
