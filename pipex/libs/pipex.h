6c7660f96545b3b95d531e03ddb5048b7c946d1c
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-08 11:14:12 -0500

    Makefile listo
    Solo esta el modo básico, pero todabía falta agregar la librería

---
+++ pipex/libs/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 10:42:51 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/07 18:09:20 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void	ft_error_message(void);
+void	ft_check_parameters(int argc, char const *argv[], char *envp[]);
+size_t	ft_strlen(const char *s);
+
+#endif
