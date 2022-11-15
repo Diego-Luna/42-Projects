4450802868fe1349bc33172e010ab634992ea15d
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-15 17:32:10 -0500

    Iniciando con so_long

---
+++ so_long/libs/so_long.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 13:06:55 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/15 16:00:48 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <mlx.h>
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+
+#define W_MAX 1600
+#define H_MAX 800
+
+#endif
---
+++ so_long/objs/main.d
@@ -0,0 +1,2 @@
+objs/main.o: src/main.c libs/so_long.h libs/libft/libft.h \
+  /usr/local/include/mlx.h
