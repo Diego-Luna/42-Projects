d9f70364b8defaec3086aaacdb56327abbcbfde5
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-17 18:15:15 -0500

    Listo

---
+++ so_long/libs/libft/src/ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:30:38 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/08 10:47:37 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_element;
+
+	new_element = malloc(sizeof(t_list));
+	if (!(new_element))
+	{
+		return (NULL);
+	}
+	new_element->content = content;
+	new_element->next = NULL;
+	return (new_element);
+}
---
+++ so_long/libs/libft/src/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:31:31 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/08 10:47:37 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t			i;
+	unsigned char	*src1;
+
+	i = 0;
+	src1 = (unsigned char *)src;
+	if (!dst && !src)
+		return (NULL);
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = src1[i];
+		i++;
+	}
+	return ((void *)dst);
+}
