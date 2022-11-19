9637fb09b1b2a54a90cfbaea26108a85a62b819d
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-19 18:39:00 -0500

    Ya genero el stack a, y listo el free

---
+++ push_swap/libs/libft/src/ft_lstadd_back.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:29:15 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/08 10:47:37 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_elem;
+
+	if (!new)
+	{
+		return ;
+	}
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last_elem = ft_lstlast(*lst);
+	last_elem->next = new;
+}
---
+++ push_swap/libs/push_swap.h
@@ -0,0 +1,12 @@
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+
+#endif
