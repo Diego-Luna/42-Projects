ebfa7b001720fabf92b7931a65a67d40cbff2f25
Diego Luna <49794452+Diego-Luna@users.noreply.github.com>
2022-11-25 18:54:20 -0500

    Trabajando en la documentación

---
+++ libs/push_swap.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 18:34:41 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/25 18:21:42 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+// # include <stdio.h>
+# include <errno.h>
+
+typedef struct s_state
+{
+	struct s_list*	l_a;
+	struct s_list*	l_b;
+	int chunk;
+	int addchunk;
+	int index;
+}	t_state;
+
+// Exit
+void ft_exit();
+
+// control
+void	ft_arrange_2_format(t_state *state);
+void	ft_arrange_3_format(t_state *state);
+void	ft_arrange_5_format(t_state *state);
+
+// utils
+t_list *ft_get_list_n(t_list *lst, int num);
+
+// get de value in int
+int	ft_gnl(t_list *lst, int num);
+int	ft_check_organize(t_list *lst, int start, int end);
+
+void	ft_print_list(t_list *lst);
+
+// Movements
+void	ft_mov_sb(t_state *state);
+void	ft_mov_sa(t_state *state);
+void	ft_mov_ss(t_state *state);
+
+void	ft_mov_pa(t_state *state);
+void	ft_mov_pb(t_state *state);
+
+void	ft_mov_ra(t_state *state);
+void	ft_mov_rb(t_state *state);
+void	ft_mov_rr(t_state *state);
+
+void	ft_mov_rra(t_state *state);
+void	ft_mov_rrb(t_state *state);
+void	ft_mov_rrr(t_state *state);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,223 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 16:38:37 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/11/25 18:48:15 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// This function receives the parameters of the program call, and
+// checks if there are repeated numbers, returns 1 when they are
+// repeated and 0 when they are not.
+int ft_repeating_numbers(char **argv)
+{
+	int	i;
+	int	ii;
+
+	i = 0;
+	while (argv[++i] && argv[i + 1])
+	{
+		ii = 0;
+		while (argv[i + ++ii])
+			if (ft_atoi(argv[i]) == ft_atoi(argv[i + ii]))
+				return (1);
+	}
+	return (0);
+}
+
+// This function is responsible for checking the parameters
+// of the program call
+int	ft_check_params(char **argv)
+{
+	int	i;
+	int	ii;
+
+	i = 1;
+	while (argv[i])
+	{
+		ii = 0;
+		if (argv[i][ii] == '-')
+			ii++;
+		while (argv[i][ii])
+		{
+			if (ft_isdigit(argv[i][ii]) == 0)
+				return (0);
+			ii++;
+		}
+		if (ii < 1)
+			return (0);
+		i++;
+	}
+	if (i < 1)
+		return (0);
+	if (ft_repeating_numbers(argv) == 1)
+	{
+		return (0);
+	}
+	return (1);
+}
+
+// This function creates a new tlist, and puts it at the end
+// of the list, receives the list to add to and the memory
+// address of the value to add to the new tlist
+void	ft_lstlast_new_table(t_list *lst, void *content)
+{
+	t_list	*tmp;
+
+	tmp = ft_lstnew(content);
+	ft_lstadd_back(&lst, tmp);
+}
+
+// This function returns the tlist at the indicated position,
+// receives the tlist where to search and the position
+t_list	*ft_get_list_n(t_list *lst, int num)
+{
+	int	i;
+
+	i = 1;
+	if (num == 0)
+	{
+		num = 1;
+	}
+	while (lst && i <= num)
+	{
+		if (i == num)
+		{
+			return (lst);
+		}
+		i++;
+		lst = lst->next;
+	}
+	if (i == num)
+	{
+		return (lst);
+	}
+	return (NULL);
+}
+
+// This function returns the int value of the tlist at the
+// indicated position, receives the tlist where to search
+// and the position
+int	ft_gnl(t_list *lst, int num)
+{
+	int	value;
+
+	value = *(int *)(ft_get_list_n(lst, num))->content;
+	return (value);
+}
+
+// This function checks that they are organized from smallest
+// to largest in the tlist, receives the starting and ending
+// point. It returns a 1 if it is organized and if it is
+// not it is 0.
+int	ft_check_organize(t_list *lst, int start, int end)
+{
+	if (start <= 0)
+	{
+		start = 1;
+	}
+	while (start < end)
+	{
+		if (ft_gnl(lst, start) > ft_gnl(lst, start + 1))
+		{
+			return (0);
+		}
+		start++;
+	}
+	return (1);
+}
+
+// We make a whole tlist free.
+void	ft_free_tlist(t_list *lst)
+{
+	t_list	*tmp;
+
+	if (!lst)
+	{
+		return ;
+	}
+	while (lst)
+	{
+		tmp = lst->next;
+		free(lst->content);
+		free(lst);
+		lst = tmp;
+	}
+}
+
+// We print in the terminal all the values of our test.
+void	ft_print_list(t_list *lst)
+{
+	while (lst)
+	{
+		ft_printf("\n data:{%d}", *(int *)lst->content);
+		lst = lst->next;
+	}
+}
+
+// This function creates a list with a table of strings.
+// Receives the status of our project and the string
+// of parameters.
+void	ft_create_lists(t_state *state, char **argv)
+{
+	int		i;
+	int		*num;
+	t_list	*tmp;
+
+	i = 1;
+	num = malloc(sizeof(int));
+	*num = ft_atoi(argv[i]);
+	state->l_a = ft_lstnew((void *)num);
+	i++;
+	while (argv[i])
+	{
+		num = malloc(sizeof(int *));
+		*num = ft_atoi(argv[i]);
+		tmp = ft_lstnew((void *)num);
+		ft_lstadd_back(&state->l_a, tmp);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_state	state;
+	int zise;
+
+	state.l_a = NULL;
+	state.l_b = NULL;
+	zise =  argc - 1;
+	if (argc > 2 && ft_check_params(argv) == 1)
+	{
+		ft_create_lists(&state, argv);
+		if (zise <= 2)
+		{
+			ft_arrange_2_format(&state);
+		}
+		else if (zise <= 3)
+		{
+			ft_arrange_3_format(&state);
+		}
+		else if (zise <= 5)
+		{
+			ft_arrange_5_format(&state);
+		}
+		else
+		{
+			ft_arrange_5_format(&state);
+		}
+		ft_free_tlist(state.l_a);
+		ft_free_tlist(state.l_b);
+	}
+	else
+	{
+		ft_exit();
+	}
+	return (0);
+}
