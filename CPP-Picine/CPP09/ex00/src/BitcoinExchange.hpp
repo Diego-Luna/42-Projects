c40896bb47f9066e67548c9dd7b0e6a4a4b20709
Diego francisco Luna lopez <diegofcolunalopez@gmail.com>
2023-05-19 19:15:37 -0400

    start CPP09

---
+++ CPP-Picine/CPP09/ex00/src/BitcoinExchange.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 18:11:28 by dluna-lo          #+#    #+#             */
+/*   Updated: 2023/05/19 19:14:32 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+#include <stack>
+#include <algorithm>
+
+class BitcoinExchange
+{
+  public:
+		BitcoinExchange(void);
+
+  private:
+};
+
+
+#endif
